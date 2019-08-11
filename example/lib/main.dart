08f0ef287b28615466588eca4c72ffd2323b1c8f
Dan Panaite <dan.panaite@live.ca>
2019-08-10 17:19:31 -0700

    Renaming the package file to flutter_radar_chart to remove pub dev warning

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_radar_chart/radar_chart.dart';
+import 'package:flutter_radar_chart/flutter_radar_chart.dart';
 
 void main() => runApp(MyApp());
 
